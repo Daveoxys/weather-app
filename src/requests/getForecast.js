6210a6d189f78bf1548abca174143e02a112375f
Dave Taylor <davidtaylor970@googlemail.com>
2022-02-08 14:24:46 +0000

    App completed css to be added

--- src/requests/getForecast.js
+++ src/requests/getForecast.js
@@ -1,13 +1,37 @@
 import axios from "axios";
 
-const getForecast = (setSelectedDate, setForecasts, setLocation) => {
-  const endpoint = "https://mcr-codes-weather-app.herokuapp.com/forecast";
+const getForecast = (
+  searchText,
+  setSelectedDate,
+  setForecasts,
+  setLocation,
+  setErrorMessage
+) => {
+  let endpoint = "https://mcr-codes-weather-app.herokuapp.com/forecast";
 
-  axios.get(endpoint).then((response) => {
-    setSelectedDate(response.data.forecasts[0].date);
-    setForecasts(response.data.forecasts);
-    setLocation(response.data.location);
-  });
+  if (searchText) {
+    endpoint += `?city=${searchText}`;
+  }
+  axios
+    .get(endpoint)
+    .then((response) => {
+      setSelectedDate(response.data.forecasts[0].date);
+      setForecasts(response.data.forecasts);
+      setLocation(response.data.location);
+    })
+    .catch((error) => {
+      const { status } = error.response;
+      if (status === 404) {
+        setErrorMessage(
+          "If that was a real place, you would be looking at the forecast right now. Take a brolly anyway just in case"
+        );
+        console.error("Location is not valid", error);
+      }
+      if (status === 500) {
+        setErrorMessage("Oops, server error, try again later.");
+        console.error("Server error", error);
+      }
+    });
 };
 
 export default getForecast;
